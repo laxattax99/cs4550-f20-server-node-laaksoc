ece810ff8e4e2076b2e8a070fed5af235b87c331
Colin Laakso <laxattack99@live.com>
2020-11-24 13:24:30 -0500

    added files to run the server

---
+++ services/questions-service.js
@@ -0,0 +1,9 @@
+let questions = require("./questions.json");
+
+const findAllQuestions = () => questions;
+const findQuestionsForQuiz = (qid) => questions.filter((q) => q.quizId === qid);
+
+module.exports = {
+  findAllQuestions,
+  findQuestionsForQuiz,
+};
